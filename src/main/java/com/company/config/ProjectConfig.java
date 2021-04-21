b62116aa926e916be1d563082cdb803ca5ae30d6
akash073 <mahmud.hasan@dsinnovators.com>
2021-04-21 10:32:18 +0600

    Localization added with header

--- src/main/java/com/company/config/ProjectConfig.java
+++ src/main/java/com/company/config/ProjectConfig.java
@@ -4,9 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class ProjectConfig {
@@ -30,5 +35,22 @@ public class ProjectConfig {
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
         bean.setOrder(Integer.MIN_VALUE);
         return bean;
+    }
+
+    //configuring default locale
+    @Bean
+    public LocaleResolver localeResolver()
+    {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+    //configuring ResourceBundle
+    @Bean
+    public ResourceBundleMessageSource messageSource()
+    {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 }
--- src/main/java/com/company/controller/IndexController.java
+++ src/main/java/com/company/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.company.controller;
 
 import com.company.dto.LoginDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class IndexController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping(path = "/",method = RequestMethod.GET)
     public String index(){
@@ -23,5 +28,13 @@ public class IndexController {
     @RequestMapping(path = "/index",method = RequestMethod.GET)
     public String hello(){
         return "index";
+    }
+
+    //internationalization
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false,defaultValue = "us") String localeString)
+    {
+        Locale locale = new Locale(localeString);
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
